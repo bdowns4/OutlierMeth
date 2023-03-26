e85a1d074d3159b61958440649c0f06b7ffabf2e
bdowns4 <63022377+bdowns4@users.noreply.github.com>
2023-03-26 16:32:07 -0400

    Add files via upload

---
+++ R/flagMeth.R
@@ -0,0 +1,21 @@
+flagMeth <-
+function(beta,reference=(all), p=(0.01)) {
+
+options(scipen = 999)
+
+binDat=matrix(rep(NA,prod(dim(beta))),ncol=ncol(beta))
+
+rownames(binDat)=rownames(beta)
+        colnames(binDat)=colnames(beta)
+
+   
+        #### get common CpGs
+        cpgs=intersect(rownames(beta),rownames(reference))
+
+binDat[cpgs,]=sweep(beta[cpgs,],1, reference[cpgs,paste0("P",p)],">")
+binDat[cpgs,][which(sweep(beta[cpgs,],1,reference[cpgs,paste0("P",p)],">"))]=1
+binDat[cpgs,][which(sweep(beta[cpgs,],1,reference[cpgs,paste0("N",p)],"<"))]=-1
+
+return(binDat)
+    }
+
